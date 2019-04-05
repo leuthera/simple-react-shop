f4ccef812ac800da7158b81bac3cc567f4a1c7d9
leuthera <contact@leuthera.com>
2019-04-05 08:28:49 +0200

    Adding ContextProvider to render ShoppingCart
    The cart requires a context, maybe we can mock this later

--- src/__tests__/shoppingCart.t.js
+++ src/__tests__/shoppingCart.t.js
@@ -1,9 +1,15 @@
-/* import React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import ShoppingCart from "../components/ShoppingCart/ShoppingCart";
+import ContextProvider from "../components/ContextProvider/ContextProvider";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<ShoppingCart />, div);
+  ReactDOM.render(
+    <ContextProvider>
+      <ShoppingCart />
+    </ContextProvider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
-}); */
+});
